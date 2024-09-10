605371bc37f3fb341edfb9772821934aef0f377e
WTFPUn <punn_namw@hotmail.com>
2024-09-10 14:59:05 +0700

    add logger into the project

---
+++ DataEngine/log.py
@@ -0,0 +1,10 @@
+import logging
+
+logging.basicConfig(
+    format="[%(filename)s:%(funcName)s] [%(levelname)s] %(message)s", level=logging.INFO,
+    handlers=[logging.FileHandler("log.log"), logging.StreamHandler()]
+)
+
+logger = logging.getLogger(__name__)
+
+logger.info("Initiated logger")

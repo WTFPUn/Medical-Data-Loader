6a3bcc0ef7f9f1ec4e217979e16c33d1b350f8b2
WTFPUn <punn_namw@hotmail.com>
2024-09-10 15:41:04 +0700

    update metadata generating

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 from DataEngine import *
-
 # sample_file("test_data", 100)
 
-get_split_meta_data("test_data", "test_output.json", [0.8, 0.1, 0.1])
+# get_split_meta_data("dataset", "split.json", [0.8, 0.1, 0.1])
+

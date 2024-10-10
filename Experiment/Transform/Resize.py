1e3755cc112a9cc45492d75dc283ec71c8727857
WTFPUn <punn_namw@hotmail.com>
2024-10-10 10:25:17 +0700

    fix Resize process

--- Experiment/Transform/Resize.py
+++ Experiment/Transform/Resize.py
@@ -2,12 +2,15 @@ from torchio.transforms import Resize as ResizeTorchio
 from typing import Tuple, Dict, Literal
 import torch
 
+
 class Resize:
     def __init__(self, target_shape: Tuple[int, int, int]):
         self.target_shape = target_shape
         self.resize = ResizeTorchio(target_shape)
 
-    def __call__(self, transform_data: Tuple[torch.tensor, ...]) -> Tuple[torch.tensor, torch.tensor]:
-        
-        data, label = self.resize(transform_data[0]), self.resize(transform_data[0])
+    def __call__(
+        self, transform_data: Tuple[torch.tensor, ...]
+    ) -> Tuple[torch.tensor, torch.tensor]:
+
+        data, label = self.resize(transform_data[0]), self.resize(transform_data[1])
         return data, label
